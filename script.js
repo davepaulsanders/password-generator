d08792098bf18ab00449714ae2d798c8a442df87
Dave Sanders <davepaulsanders@gmail.com>
2022-03-05 18:01:24 -0500

    Added README and comments

--- script.js
+++ script.js
@@ -1,30 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Global variables for prompts
 let length;
 let upperCase;
 let lowerCase;
 let numericChar;
 let specialChar;
+// Character lists for different prompt choices
+
 const upperCaseList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCaseList = "abcdefghijklmnopqrstuvwxyz";
 const numberList = "0123456789";
 const specialList = "!\"#$%&()'*+,-./:;<=>?@[\\]^_`{|}~";
+// Empty array to push character lists that have been chosen
 
-let charList = [];
+let passwordCharList = [];
+
 // Write password to the #password input
 function writePassword() {
   length = window.prompt(
     "How long do you want your password to be? (Between 8 and 128 characters"
   );
 
+  // Recalling function if answer is not valid
   if (length === "" || length < 8 || length > 128) {
     window.alert("Choose a number between 8 and 128!");
+
     writePassword();
   }
   characterChoice();
   window.alert("Generating your password!");
+
   var password = generatePassword();
-  console.log(password);
+
+  // Making password visible on page
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
@@ -32,39 +41,55 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-  let passwordString = "";
-  for (let i = 0; i < length; i++) {
-    let randomObjects = Math.floor(Math.random() * (charList.length - 1 + 1));
-    const stringLength = charList[randomObjects].length;
-    const randomStringChar = Math.floor(Math.random() * stringLength);
-    passwordString = passwordString + charList[randomObjects][randomStringChar];
-  }
-  return passwordString;
-}
-
 function characterChoice() {
-  charList = [];
+  // Emptying passwordCharList if password has previously been generated without reloading page
+  passwordCharList = [];
+
+  //Pushing character lists to passwordCharList array if they were selected
+
   upperCase = window.confirm("Would you like to use uppercase characters?");
   if (upperCase) {
-    charList.push(upperCaseList);
+    passwordCharList.push(upperCaseList);
   }
   lowerCase = window.confirm("Would you like to use lowercase characters?");
   {
     if (lowerCase) {
-      charList.push(lowerCaseList);
+      passwordCharList.push(lowerCaseList);
     }
   }
   numericChar = window.confirm("Would you like to use numeric characters?");
   if (numericChar) {
-    charList.push(numberList);
+    passwordCharList.push(numberList);
   }
   specialChar = window.confirm("Would you like to use special characters?");
   if (specialChar) {
-    charList.push(specialList);
+    passwordCharList.push(specialList);
   }
+
+  // If nothing is selected, recall the function
   if (!upperCase && !lowerCase && !numericChar && !specialChar) {
     window.alert("You have to chose at least one type of character!");
     characterChoice();
   }
+}
+
+function generatePassword() {
+  // Initializing empty string for password
+
+  let passwordString = "";
+
+  for (let i = 0; i < length; i++) {
+    // Choosing a random index from the passwordCharList array
+    let randomObjects = Math.floor(
+      Math.random() * (passwordCharList.length - 1 + 1)
+    );
+    // Finding the length of the string at the chosen index
+    const stringLength = passwordCharList[randomObjects].length;
+    // Choosing a random character from that string
+    const randomStringChar = Math.floor(Math.random() * stringLength);
+    //Adding that string to the end of the temporary passwordString
+    passwordString =
+      passwordString + passwordCharList[randomObjects][randomStringChar];
+  }
+  return passwordString;
 }
